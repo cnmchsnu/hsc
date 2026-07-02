037c50e25705e932eb96ef8453f91a7a466c78e4
ip192htwhsnu <11410541@gs.hs.ntnu.edu.tw>
2026-07-02 11:39:24 +0800

    another vercel test

--- apps/shop/src/proxy.ts
+++ apps/shop/src/proxy.ts
@@ -1,8 +1,8 @@
 import { type NextRequest } from "next/server";
 
-import { updateSession } from "@repo/database/middleware";
+import { updateSession } from "@repo/database/proxy";
 
-export async function middleware(
+export async function proxy(
     request: NextRequest,
 ) {
     console.log("====== MIDDLEWARE ENV TEST ======");
